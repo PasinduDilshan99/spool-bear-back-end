04ae8671ba660dac52e031477589e8829994ccad
PasinduDilshan99 <pd.dimbulana@gmail.com>
2026-07-04 11:26:09 +0530

    add

--- src/main/java/com/spoolbear/config/SecurityConfig.java
+++ src/main/java/com/spoolbear/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://api-test.spoolbear.com","https://test.spoolbear.com"));
+        configuration.setAllowedOrigins(List.of("https://api.spoolbear.com","https://spoolbear.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
